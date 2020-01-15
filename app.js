71a6df310bcc69d781dab7a91deb1a18425daad6
johnfruscella <johnfruscella@gmail.com>
2020-01-14 23:01:06 -0500

    modified package.json file

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // Importing the package
+const app = express();// Execute the package
 const mongoose = require('mongoose');
+const port = 3000;
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to the root route of my web server!!!!!');
 })
-app.get('/careerdevs', (req,res) => {
+app.get('/careerdevs', (req, res) => {
     res.send('Welcome to CareerDevs! Yay!');
 })
+
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 };
 
-mongoose.connect('mongodb+srv://johnf:skippy04@cluster0-pyvqf.mongodb.net/test?retryWrites=true&w=majority',options, () => {
+mongoose.connect('mongodb+srv://johnf:skippy04@cluster0-pyvqf.mongodb.net/test?retryWrites=true&w=majority', options, () => {
     console.log('Connected to MongoDB')
 });
 
-const port = 3000;
-app.listen(port, () =>{
+
+app.listen(port, () => {
     console.log(`listening to port ${port}`);
 });
 
